16a00d25878d1435978ef019121f90b6ec5b76b7
Gliddd4 <152733102+Gliddd4@users.noreply.github.com>
2024-12-30 15:30:29 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/IMG_4153.png',
+        '/IMG_4192.jpeg',
+        // Add other assets like CSS, JS, etc.
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
